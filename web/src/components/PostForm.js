8a504e396cb402a8e17f3c4feddde0b5c62e87de
ichorrr <ichorrr@gmail.com>
2023-10-18 22:14:02 +0300

    add something

--- web/src/components/PostForm.js
+++ web/src/components/PostForm.js
@@ -22,7 +22,11 @@ const Form = styled.form`
   }
 `;
 
-
+const SimpleMDE = styled.textarea`
+  width: 95%;
+  height: 200px;
+  font-size: 2.5em;
+`;
 
 const PostForm = props => {
   // set the default state of the form\
