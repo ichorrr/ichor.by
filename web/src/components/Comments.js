3053f32b0b9dd49fe19887907af52be27077fbaf
ichorrr <ichorrr@gmail.com>
2025-01-01 18:25:11 +0300

    add commits empty inf

--- web/src/components/Comments.js
+++ web/src/components/Comments.js
@@ -1,19 +1,8 @@
 import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
-import { format } from 'date-fns';
-import { GET_COMMENTS } from '../gql/query';
 
 const Comments = props =>  {
     const [text, setText] = useState("");
     const [comments, setComments] = useState([]);
-
-    const { data, loading, error } = useQuery(GET_COMMENTS, {
-      variables: {
-        post: props.post._id
-      },
-    });
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error!</p>;
 
     const onClickHandler = () => {
         setComments((comments) => [...comments, text])
@@ -23,19 +12,6 @@ const Comments = props =>  {
         setText(e.target.value);
       };
 
-<<<<<<< HEAD
-      const postcom = props.post;
-      const arrcom = data.getComments;
-      console.log(data.getComments)
-return (
-  <>
-  <div className='comments-block'>
-
-      <div className='top_commbocks'>
-        <h3>Комментарии к записи</h3>
-              <span className='length-comments'>всего {postcom.comments.length} комментариев</span>
-      </div>
-=======
       
       console.log(text);
       console.log(props.post);
@@ -54,10 +30,10 @@ return (
         ))}
         <h3>Комментарии к записи</h3>
         <span className='length-comments'>всего {postcom.length} комментариев</span>
->>>>>>> e92ec8683406a2d83f273207fbea01eadc64ef49
 
         <form onSubmit={event => {
           event.preventDefault();
+
           props.action({
             variables: {
               text,
@@ -65,26 +41,13 @@ return (
             }
           });
         }}>
-        <textarea value={text} onChange={onChangeHandler} placeholder="Введите текст">Текст комментария</textarea>
+        <textarea value={text} onChange={onChangeHandler}>Текст комментария</textarea>
         <button type="submit"  value={comments} onClick={onClickHandler}>Опубликовать</button>
+    
         </form>
 
-     <div>
-
-        {arrcom.map(({text}) => (
-            <div className='comment-block'  key={text.id}>
-              <div>
-                <h3>{text.post}</h3>
-                <span>{format(new Date(postcom.createdAt), 'dd LLL yyyy  HH:mm')}</span>
-              </div>
-              <p>{text}</p>
-            </div>    
-        ))}
     </div>
-    </div>
-    </>
 )
 };
 
 export default Comments;
-
